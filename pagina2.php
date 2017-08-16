8723401dce67a45384b5ee06bea3e8a8c8ec79a8
jose aldair nava correa <aldair-zaragoza@hotmail.com>
2017-08-16 08:41:23 -0500

    resultados en cambios

--- pagina2.php
+++ pagina2.php
@@ -1,1 +1,10 @@
+<HTML>
+<HEAD>
+</HEAD>
+<BODY>
+<h1>Jose Aldair Nava Correa</h1>
+<h2>Programacion visual</h2>
 <a href="index.php">inicio</a>
+<a href="pagina3.php">Pagina 3</a>
+</BODY>
+</HTML>
